e8b1959ac13acf11ae1acbcb1eeb5430b9a55f8d
HarmonyEarth <sentai@chowa.dev>
2022-01-24 13:00:12 -0500

    Update API endpoints to V2 Coinranking API and remove exchanges

--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,55 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import millify from 'millify';
-import { Link } from 'react-router-dom';
-import { Card, Row, Col, Input, Typography } from 'antd';
+import React, { useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input, Typography } from "antd";
 
-import { useGetCoinsQuery } from '../services/coinApi';
-import Loader from './Loader';
+import { useGetCoinsQuery } from "../services/coinApi";
+import Loader from "./Loader";
 
 const { Title } = Typography;
 
-const Coins = ( {simplified} ) => {
-    const count = simplified ? 10 : 100;
-    const { data: coinsList, isFetching } = useGetCoinsQuery(count);
-    const [coins, setCoins] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+const Coins = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: coinsList, isFetching } = useGetCoinsQuery(count);
+  const [coins, setCoins] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
+  useEffect(() => {
+    const filteredData = coinsList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm)
+    );
 
-        const filteredData = coinsList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm));
+    setCoins(filteredData);
+  }, [coinsList, searchTerm]);
 
-        setCoins(filteredData);
-    }, [coinsList, searchTerm]);
+  console.log("mua", coins);
+  if (isFetching) return <Loader />;
 
-    console.log(coins)
-    if (isFetching) return <Loader />;
+  return (
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Title level={2} style={{ textAlign: "center" }}>
+            {" "}
+            Coins{" "}
+          </Title>
+          <Input
+            placeholder="Search Coins"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {coins?.map((coin) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={coin.uuid}>
+            <Link to={`/coin/${coin.uuid}`}>
+              <Card
+                title={`${coin.rank}. ${coin.name}`}
+                extra={
+                  <img
+                    className="crypto-image"
+                    src={coin.iconUrl}
+                    alt={`${coin.name} Logo`}
+                  />
+                }
+                hoverable
+              >
+                <p style={{ fontWeight: "500" }}>
+                  {" "}
+                  Price: {millify(coin.price)}{" "}
+                </p>
+                <p style={{ fontWeight: "500" }}>
+                  {" "}
+                  Market Cap: {millify(coin.marketCap)}{" "}
+                </p>
+                <p style={{ fontWeight: "500" }}>
+                  {" "}
+                  Daily Change: {millify(coin.change)}%{" "}
+                </p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-    return (
-        <>
-            
-            {!simplified && (
-                
-                <div className='search-crypto'><Title level={2} style={{ textAlign:'center' }} > Coins </Title>
-                    <Input placeholder='Search Coins' onChange={(e) => setSearchTerm(e.target.value)} />
-                </div>
-            )}
-            <Row gutter={[32, 32]} className='crypto-card-container'>
-                {coins?.map((coin) => (
-                    <Col xs={24} sm={12} lg={6} className='crypto-card' key={coin.id}>
-                        <Link to={`/coin/${coin.id}`}>
-                            <Card title={`${coin.rank}. ${coin.name}`} 
-                            extra={<img className='crypto-image' src={coin.iconUrl} alt={`${coin.name} Logo`}  /> }
-                            hoverable >
-                                <p style={{fontWeight:'500'}}> Price: {millify(coin.price)} </p>
-                                <p style={{fontWeight:'500'}}> Market Cap: {millify(coin.marketCap)} </p>
-                                <p style={{fontWeight:'500'}}> Daily Change: {millify(coin.change)}% </p>
-                            </Card>
-                        </Link>
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
-}
-
-export default Coins
+export default Coins;
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,67 +1,76 @@
-import React from 'react';
-import { Chart as ChartJS } from 'chart.js/auto';
-import { Line } from 'react-chartjs-2';
-import { Col, Row, Typography } from 'antd';
+import React from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+import { Col, Row, Typography } from "antd";
 
 const { Title } = Typography;
 
+const LineChart = ({ coinHistory, currentPrice, coinName }) => {
+  const coinPrice = [];
+  const coinTimestamp = [];
 
+  for (let i = coinHistory?.data?.history?.length - 1; i >= 0; i--) {
+    coinPrice.push(coinHistory?.data?.history[i].price);
+  }
 
-const LineChart = ({coinHistory, currentPrice, coinName}) => {
+  for (let i = coinHistory?.data?.history?.length - 1; i >= 0; i--) {
+    coinTimestamp.push(
+      //   new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString(
+      //     "en-US"
+      //   )
+      new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+      }).format(coinHistory?.data?.history[i].timestamp * 1000)
+    );
+  }
 
-    const coinPrice = [];
-    const coinTimestamp = [];
+  const data = {
+    labels: coinTimestamp,
+    datasets: [
+      {
+        label: "Price (USD)",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
 
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinPrice.push(coinHistory?.data?.history[i].price);
-    }
-
-    for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-        coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
-    }
-
-    const data = {
-        labels: coinTimestamp,
-        datasets: [
+  const options = {
+    scales: {
+      yAxes: [
         {
-            label: 'Price (USD)',
-            data: coinPrice,
-            fill: false,
-            backgroundColor: '#0071bd',
-            borderColor: '#0071bd',
+          ticks: {
+            beginAtZero: true,
+          },
         },
-        ],
-    };
+      ],
+    },
+  };
 
-    const options = {
-        scales: {
-        yAxes: [
-            {
-            ticks: {
-                beginAtZero: true,
-            },
-            },
-        ],
-        },
-    };
+  return (
+    <>
+      <Row className="chart-header">
+        <Title level={2} className="chart-title">
+          {" "}
+          {coinName} Price Chart{" "}
+        </Title>
+        <Col className="price-container">
+          <Title level={5} className="price-change">
+            Change: {coinHistory?.data?.change}%
+          </Title>
+          <Title level={5} className="current-price">
+            Current {coinName} Price: ${currentPrice}
+          </Title>
+        </Col>
+      </Row>
+      <Line data={data} options={options} />
+    </>
+  );
+};
 
-
-    return (
-        <>
-            <Row className='chart-header'>
-                <Title level={2} className='chart-title'> {coinName} Price Chart </Title>
-                <Col className='price-container'>
-                    <Title level={5} className='price-change'>
-                        Change: {coinHistory?.data?.change}%
-                    </Title>
-                    <Title level={5} className='current-price'>
-                        Current {coinName} Price: ${currentPrice}
-                    </Title>
-                </Col>
-            </Row> 
-            <Line data={data} options={options} />
-        </>
-    )
-}
-
-export default LineChart
+export default LineChart;
